74fd0d7b0ad0a9c34128107e2019003bc00f5e3f
Le Ngoc Hung <hungln73@lgcns.com>
2024-10-29 17:26:18 +0900

    Build-and-add-navbar-component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/frontend_assets/assets'
+const Navbar = () => {
+
+    const [menu,setMenu] = useState("home")
+
+    return (
+        <div className='navbar'>
+            <img src={assets.logo_2} alt="" className="logo" />   
+            <ul className="navbar-menu">
+                <li onClick={() => setMenu("home")}  className={menu === "home" ? "active" : ""}>Home</li>    
+                <li onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</li>    
+                <li onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>Mobile-app</li>    
+                <li onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>Contact-us</li>    
+            </ul>
+            <div className="navbar-right">
+                <img src={assets.search_icon} alt="" className="" />   
+                <div className="navbar-search-icon">
+                    <img src={assets.basket_icon} alt="" className="" />    
+                    <div className="dot">
+                    </div>
+                </div> 
+                <button className='navbar-button'>Sign in</button>
+            </div> 
+        </div>   
+    )
+}
+
+export default Navbar
